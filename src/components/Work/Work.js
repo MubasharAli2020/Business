18a243ceac9bcedf1a2a4025dbc4db15709f80bb
MubasharAli2020 <22mubasharali@gmail.com>
2022-10-15 09:55:04 +0500

    changes

--- src/components/Work/Work.js
+++ src/components/Work/Work.js
@@ -15,10 +15,10 @@ function Work() {
        <main class="page-content">
   <div class="card">
     <div class="content">
-      <h2 class="services_title">Graphic Design || Social Media Management</h2>
+      <h2 class="services_title">Social Media Management</h2>
       <p class="copy">We will provide you Full design posts that you can post on your all Social Media Platforms.</p>
       {/* <button class="btn">View Trips</button> */}
-      <p class="services_btn">Beautiful Posts For all Social Media Platforms</p>
+      {/* <p class="services_btn">Beautiful Posts For all Social Media Platforms</p> */}
       <p class="services_btn">With Popular Hastags to Target Audience</p>
     </div>
   </div>
@@ -26,18 +26,18 @@ function Work() {
     <div class="content">
       <h2 class="services_title">Website Developement</h2>
       <p class="copy">If your Business doesn't have Website then we will make a complete Website and we will manage it on regular Basis.</p>
-        <p class="services_btn">Website Developement From Scratch</p>
+        {/* <p class="services_btn">Website Developement From Scratch</p> */}
         <p class="services_btn">Website Maintenance on Daily Basis</p>
       {/* <button class="btn">View Trips</button> */}
     </div>
   </div>
   <div class="card">
     <div class="content">
-      <h2 class="services_title">BOOST YOUR SALES WITH ChatBot Intergeration </h2>
+      <h2 class="services_title">ChatBot Intergeration </h2>
       <p class="copy">We will integerate Chatbot to your Website and Social Media Platforms and will answer the customer Queries 24 /7 .</p>
       {/* <button class="btn">View Trips</button> */}
-      <p class="services_btn">ChatBot Intergeration</p>
-        <p class="services_btn">Handling Customer Queries 24 / 7</p>
+      {/* <p class="services_btn">ChatBot Intergeration</p> */}
+        <p class="services_btn">Grow Sales with ChatBot</p>
     </div>
   </div>
   <div class="card">
@@ -46,7 +46,7 @@ function Work() {
       <p class="copy">We will handle your Customer Queries via call, Email, Chat or through Social Media Platforms .</p>
       {/* <button class="btn">View Trips</button> */}
       <p class="services_btn">Customer Service with Customer Satisfaction</p>
-        <p class="services_btn">Handling Customer Queries 24 / 7</p>
+        {/* <p class="services_btn">Handling Customer Queries 24 / 7</p> */}
     </div>
   </div>
   {/* <div class="card">
